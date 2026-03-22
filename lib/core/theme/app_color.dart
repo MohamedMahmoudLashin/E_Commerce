11e421de9f36e492c4fe28017928e45a4a5dde19
Mohamed Lashin <lashinm80@gmail.com>
2026-03-22 23:49:17 +0200

    add Usage Colors

---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColor {
+  static const kWhite0 = Color(0xffFFFFFF);
+  static const kRed0 = Color(0xffF83758);
+  static const kBlue0 = Color(0xff3D4DA6);
+  static const kBlue1 = Color(0xff4392F9);
+  static const kBlack0 = Color(0xff000000 );
+  static const kGrey0 = Color(0xffA8A8A9 );
+  static const kPurple0 = Color(0xffFFCCD5 );
+}

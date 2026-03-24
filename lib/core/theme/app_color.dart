46a1cf6de6ee2af09f385d69490baf33acd41e93
Mohamed Lashin <lashinm80@gmail.com>
2026-03-24 20:36:40 +0200

    add color

--- lib/core/theme/app_color.dart
+++ lib/core/theme/app_color.dart
@@ -2,11 +2,14 @@ import 'dart:ui';
 
 class AppColor {
   static const kWhite0 = Color(0xffFFFFFF);
+  static const kWhite1 = Color(0xffF3F3F3);
   static const kRed0 = Color(0xffF83758);
+  static const kRed1 = Color(0xffFF4B26);
   static const kBlue0 = Color(0xff3D4DA6);
   static const kBlue1 = Color(0xff4392F9);
   static const kBlack0 = Color(0xff000000 );
   static const kGrey0 = Color(0xffA8A8A9 );
+  static const kGrey1 = Color(0xff626262);
   static const kPurple0 = Color(0xffFFCCD5 );
   static const kBlack1 = Color(0xff17223B );
 }

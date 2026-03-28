613e0210b4d2a4a844795a93ae8897676599d5cf
Mohamed Lashin <lashinm80@gmail.com>
2026-03-28 22:24:07 +0200

    add custom search text form

---
+++ lib/features/home/presentation/widget/custom_search_text_form_field.dart
@@ -0,0 +1,42 @@
+import 'package:e_commerce/core/responsive/responsive_extension.dart';
+import 'package:e_commerce/core/theme/app_color.dart';
+import 'package:e_commerce/core/widget/custom_global_text.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomSearchTextFormField extends StatelessWidget {
+  const CustomSearchTextFormField({super.key, this.validate, this.controller});
+
+  final String? Function(String?)? validate;
+  final TextEditingController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: validate,
+      controller: controller,
+      decoration: InputDecoration(
+        filled: true,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.sp),
+          borderSide: BorderSide(color: AppColor.kWhite0),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.sp),
+          borderSide: BorderSide(color: AppColor.kWhite0),
+        ),
+        hint: CustomGlobalText(
+          size: 16,
+          weight: FontWeight.w500,
+          color: AppColor.kWhite1,
+          txt: "sear1".tr(),
+        ),
+        fillColor: AppColor.kWhite0,
+        prefixIcon: SvgPicture.asset("assets/icons/search 1.svg"),
+        suffixIcon: SvgPicture.asset("assets/icons/mic.svg"),
+      ),
+    );
+  }
+}

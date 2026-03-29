5c8760ffc27b1c4ddc1b08c40985d2de28dc0403
Mohamed Lashin <lashinm80@gmail.com>
2026-03-30 00:07:10 +0200

    add custom global textButton,

---
+++ lib/core/widget/custom_global_text_button.dart
@@ -0,0 +1,34 @@
+import 'package:e_commerce/core/widget/custom_global_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomGlobalTextButton extends StatelessWidget {
+  const CustomGlobalTextButton({
+    super.key,
+    required this.txt,
+    required this.color,
+    required this.size,
+    this.press,
+    required this.weight,
+  });
+  final void Function()? press;
+  final int size;
+  final FontWeight weight;
+  final Color color;
+  final String txt;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: press,
+      child: CustomGlobalText(
+        size: size,
+        weight: weight,
+        color: color,
+        txt: txt,
+      ),
+    );
+  }
+}

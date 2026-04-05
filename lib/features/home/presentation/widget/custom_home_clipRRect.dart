fd4cdd575472ec9e22de149967a536b1cbf3d919
Mohamed Lashin <lashinm80@gmail.com>
2026-04-05 22:47:00 +0200

    edit by Change Container With SizedBox  and add press>onTap

--- lib/features/home/presentation/widget/custom_home_clipRRect.dart
+++ lib/features/home/presentation/widget/custom_home_clipRRect.dart
@@ -1,6 +1,5 @@
+import 'package:e_commerce/core/theme/app_color.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_svg/svg.dart';
-
 import '../../../../core/responsive/responsive_extension.dart';
 import '../../../../core/widget/custom_global_text.dart';
 
@@ -12,29 +11,47 @@ class CustomHomeCliprrect extends StatelessWidget {
     required this.txt,
     required this.weight,
     required this.color,
+    this.press,
+
   });
 
+  final void Function()? press;
   final String image;
 
-  final int size;
+  final double size;
   final String txt;
   final FontWeight weight;
   final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 60.w,
-          height: 60.w,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(30.sp),
-            child: SvgPicture.asset(image, fit: BoxFit.cover),
-          ),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 7.h),
+      child: SizedBox(
+        height: 110.h,
+        child: Column(
+          mainAxisAlignment: .spaceAround,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                // boxShadow:[
+                //   BoxShadow(color: AppColor.kWhite1.withAlpha(60),blurRadius:15,spreadRadius: 1,blurStyle: BlurStyle.normal,offset: Offset(2, 3) )
+                // ]
+              ),
+              width: 75.w,
+              height: 75.w,
+              child: GestureDetector(
+                onTap: press,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(40.sp),
+                  child: Image.asset(image, fit: BoxFit.cover),
+                ),
+              ),
+            ),
+            CustomGlobalText(size: size, weight: weight, color: color, txt: txt),
+          ],
         ),
-        CustomGlobalText(size: size, weight: weight, color: color, txt: txt),
-      ],
+      ),
     );
   }
 }

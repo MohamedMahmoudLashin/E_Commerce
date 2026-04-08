69dfc0f36d38ad635beac7fa13507e0e0ff3ff28
Mohamed Lashin <lashinm80@gmail.com>
2026-04-09 00:06:49 +0200

    add custom global sizedbox to getstart screens

--- lib/features/get_start/presentation/ui_screen/get_start_screen.dart
+++ lib/features/get_start/presentation/ui_screen/get_start_screen.dart
@@ -1,7 +1,8 @@
 import 'package:e_commerce/core/responsive/responsive_extension.dart';
 import 'package:e_commerce/core/theme/app_color.dart';
-import 'package:e_commerce/features/get_start/presentation/widget_screen/custom_container_text_button.dart';
-import 'package:e_commerce/features/get_start/presentation/widget_screen/custom_get_text.dart';
+import 'package:e_commerce/core/widget/custom_global_container_text_button.dart';
+import 'package:e_commerce/core/widget/custom_global_sized_box.dart';
+import 'package:e_commerce/core/widget/custom_global_text.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +15,8 @@ class GetStartScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(child: Image.asset("assets/images/Get Started.png",fit:BoxFit.cover ,)),
+          Positioned.fill(child: Image.asset("assets/images/get_Started.png",fit:BoxFit.cover ,)),
+          ////////////Blur Shadow////////////////
           Positioned.fill(
             child: Container(
               decoration: BoxDecoration(
@@ -23,7 +25,7 @@ class GetStartScreen extends StatelessWidget {
                   end: Alignment.bottomCenter,
                   colors: [
                     Colors.transparent,
-                    AppColor.kBlack0.withAlpha(140),
+                    AppColor.kBlack0.withAlpha(130),
                   ],
                 ),
               ),
@@ -36,13 +38,17 @@ class GetStartScreen extends StatelessWidget {
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 70.w,vertical: 30.h),
               child: Column(
-                crossAxisAlignment: .center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                   CustomGetText(size: 43, weight: FontWeight.w600, color: AppColor.kWhite0, txt: "t1Get".tr()),
-                  SizedBox(height: 20.h,),
-                   CustomGetText(size: 17, weight: FontWeight.w400, color: AppColor.kGrey, txt: "t2Get".tr()),
-                  SizedBox(height: 50.h,),
-                 CustomContainerTextButton()
+                   CustomGlobalText(size: 43, weight: FontWeight.w600, color: AppColor.kWhite0, txt: "t1Get".tr(),align: TextAlign.center,),
+                  CustomGlobalSizedBox(height: 20,),
+                   CustomGlobalText(size: 17, weight: FontWeight.w400, color: AppColor.kGrey, txt: "t2Get".tr(),align: TextAlign.center),
+                  CustomGlobalSizedBox(height: 50,),
+                  ///use CustomGlobalContainerTextButton
+                  CustomGlobalContainerTextButton(
+                      press:(){
+                        Navigator.of(context).pushReplacementNamed("heart");
+                      },size: 26, weight: FontWeight.w600, color: AppColor.kWhite1, txt: "getStart".tr()),
                 ],
               ),
             ),
